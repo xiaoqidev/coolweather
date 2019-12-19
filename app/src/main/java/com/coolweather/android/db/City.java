5b4e2aff0825d8e5aa59f0b6c105067435a66275
xiaoqi <491298880@qq.com>
2019-12-19 22:55:10 +0800

    新增切换城市和手动更新功能

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -1,4 +1,42 @@
 package com.coolweather.android.db;
 
-public class City {
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCithName() {
+        return cityName;
+    }
+
+    public void setCithName(String cithName) {
+        this.cityName = cithName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
 }
--- app/src/main/java/com/coolweather/android/gson/Forecast.java
+++ app/src/main/java/com/coolweather/android/gson/Forecast.java
@@ -1,4 +1,24 @@
 package com.coolweather.android.gson;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Forecast {
+    @SerializedName("date")
+    public String date;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+        @SerializedName("txt_d")
+        public String info;
+    }
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+
+    public class Temperature {
+        public String max;
+        public String min;
+    }
 }
